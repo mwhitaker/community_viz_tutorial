1edea53a84b54f5c050386d9f53fffe601fa1110
Michael Whitaker <monitus@gmail.com>
2019-11-21 12:10:58 -0800

    final product

--- src/index.js
+++ src/index.js
@@ -1,17 +1,114 @@
 const dscc = require('@google/dscc');
 const viz = require('@google/dscc-scripts/viz/initialViz.js');
 const local = require('./localMessage');
+const d3 = require('d3');
 
 
 // change this to 'true' for local development
 // change this to 'false' before deploying
+
+// https://bl.ocks.org/d3noob/402dd382a51a4f6eea487f9a35566de0
+
+// date,close
+// 1-May-12,58.13
+// 30-Apr-12,53.98
+
 export const LOCAL = true;
 
+function sortByDateAscending(a, b) {
+  return a.date - b.date;
+}
+
 // write viz code here
-const drawViz = (data) => {
-  viz.readmeViz();
-  viz.firstViz(data);
+const drawViz = (dataIn) => {
+  var margin = { top: 20, right: 20, bottom: 30, left: 50 },
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+  // parse the date / time
+  var parseTime = d3.timeParse("%d-%b-%y");
+  var parseTime2 = d3.timeParse("%Y%m%d");
+
+  // set the ranges
+  var x = d3.scaleTime().range([0, width]);
+  var y = d3.scaleLinear().range([height, 0]);
+  
+
+  // define the line
+  var valueline = d3.line()
+    .x(function (d) { var scaled = x(d.date); return scaled; })
+    .y(function (d) { return y(d.close); });
+
+  // append the svg obgect to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  var svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+      "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+  // Get the data
+  // d3.csv("data.csv").then(dataBackup => {
+
+    var tblList = dataIn.tables.DEFAULT;
+    var data = tblList.map(row => {
+      return {
+        date: parseTime2(row["dimID"][0]),
+        close: row["metricID"][0]
+      }
+    }).sort(sortByDateAscending);
+    
+    // format the data
+    // data.forEach(function (d) {
+    //   d.date = parseTime(d.date);
+    //   d.close = +d.close;
+    // });
+    
+
+
+
+    // Scale the range of the data
+    x.domain(d3.extent(data, function (d) { return d.date; }));
+    y.domain([0, d3.max(data, function (d) { return d.close; })]);
+
+    // let style = {
+    //   lineColor:
+    //   dataIn.style.lineColor.value !== undefined
+    //         ? dataIn.style.lineColor.value.color
+    //         : dataIn.style.lineColor.defaultValue,
+    //     lineWeight:
+    //     dataIn.style.lineWeight.value !== undefined
+    //         ? dataIn.style.lineWeight.value
+    //         : dataIn.style.lineWeight.defaultValue
+    //   }
+
+    // Add the valueline path.
+    svg.append("path")
+      .data([data])
+      .attr("class", "line")    // this is where the style gets set in the css file.
+      .attr("d", valueline);
+       // .attr("stroke", style.lineColor).attr("stroke-width", style.lineWeight + "px")
+
+    // Add the X Axis
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+
+    d3.axisLeft(y)(svg.append("g"))
 };
+
+
+// write viz code here
+// const drawViz = (data) => {
+//   viz.readmeViz();
+//   viz.firstViz(data);
+// };
+
+
 
 
 // renders locally
--- src/localMessage.js
+++ src/localMessage.js
@@ -5,47 +5,373 @@
  *
  */
 export const message = {
-  tables: {
-    DEFAULT: [
+  "tables": {
+    "DEFAULT": [
       {
-        dimID: ['25-54'],
-        metricID: [128863172],
+        "dimID": [
+          "20190820"
+        ],
+        "metricID": [
+          3313,
+          12766
+        ]
       },
       {
-        dimID: ['0-14'],
-        metricID: [61175933],
+        "dimID": [
+          "20190819"
+        ],
+        "metricID": [
+          2805,
+          11557
+        ]
       },
       {
-        dimID: ['65+'],
-        metricID: [51055052],
+        "dimID": [
+          "20190827"
+        ],
+        "metricID": [
+          2634,
+          11468
+        ]
       },
       {
-        dimID: ['15-24'],
-        metricID: [43351778],
+        "dimID": [
+          "20190904"
+        ],
+        "metricID": [
+          2603,
+          11531
+        ]
       },
       {
-        dimID: ['55-64'],
-        metricID: [42179856],
+        "dimID": [
+          "20190903"
+        ],
+        "metricID": [
+          2551,
+          10620
+        ]
       },
-    ],
+      {
+        "dimID": [
+          "20190822"
+        ],
+        "metricID": [
+          2491,
+          11767
+        ]
+      },
+      {
+        "dimID": [
+          "20190821"
+        ],
+        "metricID": [
+          2480,
+          11957
+        ]
+      },
+      {
+        "dimID": [
+          "20190813"
+        ],
+        "metricID": [
+          2437,
+          11624
+        ]
+      },
+      {
+        "dimID": [
+          "20190829"
+        ],
+        "metricID": [
+          2435,
+          10833
+        ]
+      },
+      {
+        "dimID": [
+          "20190809"
+        ],
+        "metricID": [
+          2425,
+          13044
+        ]
+      },
+      {
+        "dimID": [
+          "20190826"
+        ],
+        "metricID": [
+          2413,
+          11121
+        ]
+      },
+      {
+        "dimID": [
+          "20190905"
+        ],
+        "metricID": [
+          2395,
+          10373
+        ]
+      },
+      {
+        "dimID": [
+          "20190812"
+        ],
+        "metricID": [
+          2387,
+          10960
+        ]
+      },
+      {
+        "dimID": [
+          "20190828"
+        ],
+        "metricID": [
+          2375,
+          10273
+        ]
+      },
+      {
+        "dimID": [
+          "20190823"
+        ],
+        "metricID": [
+          2292,
+          10650
+        ]
+      },
+      {
+        "dimID": [
+          "20190814"
+        ],
+        "metricID": [
+          2184,
+          10292
+        ]
+      },
+      {
+        "dimID": [
+          "20190815"
+        ],
+        "metricID": [
+          2112,
+          9379
+        ]
+      },
+      {
+        "dimID": [
+          "20190816"
+        ],
+        "metricID": [
+          2083,
+          10187
+        ]
+      },
+      {
+        "dimID": [
+          "20190830"
+        ],
+        "metricID": [
+          2034,
+          9767
+        ]
+      },
+      {
+        "dimID": [
+          "20190902"
+        ],
+        "metricID": [
+          1894,
+          6957
+        ]
+      },
+      {
+        "dimID": [
+          "20190824"
+        ],
+        "metricID": [
+          1669,
+          6644
+        ]
+      },
+      {
+        "dimID": [
+          "20190810"
+        ],
+        "metricID": [
+          1640,
+          6618
+        ]
+      },
+      {
+        "dimID": [
+          "20190825"
+        ],
+        "metricID": [
+          1539,
+          6550
+        ]
+      },
+      {
+        "dimID": [
+          "20190818"
+        ],
+        "metricID": [
+          1536,
+          6876
+        ]
+      },
+      {
+        "dimID": [
+          "20190831"
+        ],
+        "metricID": [
+          1473,
+          6305
+        ]
+      },
+      {
+        "dimID": [
+          "20190901"
+        ],
+        "metricID": [
+          1455,
+          5865
+        ]
+      },
+      {
+        "dimID": [
+          "20190811"
+        ],
+        "metricID": [
+          1431,
+          6720
+        ]
+      },
+      {
+        "dimID": [
+          "20190817"
+        ],
+        "metricID": [
+          1423,
+          5785
+        ]
+      }
+    ]
   },
-  fields: {
-    dimID: [
+  "fields": {
+    "dimID": [
       {
-        id: 'qt_nzqx6a0xvb',
-        name: 'Age Group',
-        type: 'TEXT',
-        concept: 'DIMENSION',
-      },
+        "id": "qt_tlovvsnv0b",
+        "name": "Date",
+        "type": "YEAR_MONTH_DAY",
+        "concept": "DIMENSION"
+      }
     ],
-    metricID: [
+    "metricID": [
       {
-        id: 'qt_8isx6a0xvb',
-        name: 'Population',
-        type: 'NUMBER',
-        concept: 'METRIC',
+        "id": "qt_m07w4snv0b",
+        "name": "Sessions",
+        "type": "NUMBER",
+        "concept": "METRIC"
       },
-    ],
+      {
+        "id": "qt_d7628tnv0b",
+        "name": "Pageviews",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
   },
-  style: {},
+  "style": {},
+  "theme": {
+    "themeFillColor": {
+      "color": "#FFFFFF"
+    },
+    "themeFontColor": {
+      "color": "#616161"
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#4285F4"
+    },
+    "themeAccentFontColor": {
+      "color": "#EFEFEF"
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#4285F4"
+      },
+      {
+        "color": "#DB4437"
+      },
+      {
+        "color": "#F4B400"
+      },
+      {
+        "color": "#0F9D58"
+      },
+      {
+        "color": "#AB47BC"
+      },
+      {
+        "color": "#00ACC1"
+      },
+      {
+        "color": "#FF7043"
+      },
+      {
+        "color": "#9E9D24"
+      },
+      {
+        "color": "#5C6BC0"
+      },
+      {
+        "color": "#F06292"
+      },
+      {
+        "color": "#00796b"
+      },
+      {
+        "color": "#c2185b"
+      },
+      {
+        "color": "#7e57c2"
+      },
+      {
+        "color": "#03a9f4"
+      },
+      {
+        "color": "#8bc34a"
+      },
+      {
+        "color": "#fdd835"
+      },
+      {
+        "color": "#fb8c00"
+      },
+      {
+        "color": "#8d6e63"
+      },
+      {
+        "color": "#9e9e9e"
+      },
+      {
+        "color": "#607d8b"
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#e9e9e9"
+    }
+  },
+  "interactions": {}
 };
