ed382515ce2f64037a00790c32aa6a6956155e32
Michael Whitaker <monitus@gmail.com>
2019-12-09 12:45:40 -0800

    uodate_message

--- src/localMessage.js
+++ src/localMessage.js
@@ -1,51 +1,427 @@
-/**
- * This file provides the mock "data" received
- * by your visualization code when you develop
- * locally.
- *
- */
 export const message = {
-  tables: {
-    DEFAULT: [
+  "tables": {
+    "DEFAULT": [
       {
-        dimID: ['25-54'],
-        metricID: [128863172],
+        "dimID": [
+          "20191202"
+        ],
+        "metricID": [
+          18473
+        ]
       },
       {
-        dimID: ['0-14'],
-        metricID: [61175933],
+        "dimID": [
+          "20191204"
+        ],
+        "metricID": [
+          14566
+        ]
       },
       {
-        dimID: ['65+'],
-        metricID: [51055052],
+        "dimID": [
+          "20191114"
+        ],
+        "metricID": [
+          14310
+        ]
       },
       {
-        dimID: ['15-24'],
-        metricID: [43351778],
+        "dimID": [
+          "20191203"
+        ],
+        "metricID": [
+          14292
+        ]
       },
       {
-        dimID: ['55-64'],
-        metricID: [42179856],
+        "dimID": [
+          "20191126"
+        ],
+        "metricID": [
+          14290
+        ]
       },
-    ],
+      {
+        "dimID": [
+          "20191113"
+        ],
+        "metricID": [
+          14216
+        ]
+      },
+      {
+        "dimID": [
+          "20191125"
+        ],
+        "metricID": [
+          14154
+        ]
+      },
+      {
+        "dimID": [
+          "20191119"
+        ],
+        "metricID": [
+          13970
+        ]
+      },
+      {
+        "dimID": [
+          "20191120"
+        ],
+        "metricID": [
+          13921
+        ]
+      },
+      {
+        "dimID": [
+          "20191121"
+        ],
+        "metricID": [
+          13680
+        ]
+      },
+      {
+        "dimID": [
+          "20191127"
+        ],
+        "metricID": [
+          13145
+        ]
+      },
+      {
+        "dimID": [
+          "20191122"
+        ],
+        "metricID": [
+          12993
+        ]
+      },
+      {
+        "dimID": [
+          "20191118"
+        ],
+        "metricID": [
+          12730
+        ]
+      },
+      {
+        "dimID": [
+          "20191111"
+        ],
+        "metricID": [
+          12471
+        ]
+      },
+      {
+        "dimID": [
+          "20191206"
+        ],
+        "metricID": [
+          12363
+        ]
+      },
+      {
+        "dimID": [
+          "20191205"
+        ],
+        "metricID": [
+          12031
+        ]
+      },
+      {
+        "dimID": [
+          "20191112"
+        ],
+        "metricID": [
+          11472
+        ]
+      },
+      {
+        "dimID": [
+          "20191129"
+        ],
+        "metricID": [
+          11169
+        ]
+      },
+      {
+        "dimID": [
+          "20191128"
+        ],
+        "metricID": [
+          10946
+        ]
+      },
+      {
+        "dimID": [
+          "20191115"
+        ],
+        "metricID": [
+          10858
+        ]
+      },
+      {
+        "dimID": [
+          "20191124"
+        ],
+        "metricID": [
+          10503
+        ]
+      },
+      {
+        "dimID": [
+          "20191201"
+        ],
+        "metricID": [
+          9542
+        ]
+      },
+      {
+        "dimID": [
+          "20191208"
+        ],
+        "metricID": [
+          9004
+        ]
+      },
+      {
+        "dimID": [
+          "20191130"
+        ],
+        "metricID": [
+          8643
+        ]
+      },
+      {
+        "dimID": [
+          "20191123"
+        ],
+        "metricID": [
+          8619
+        ]
+      },
+      {
+        "dimID": [
+          "20191117"
+        ],
+        "metricID": [
+          8572
+        ]
+      },
+      {
+        "dimID": [
+          "20191207"
+        ],
+        "metricID": [
+          7976
+        ]
+      },
+      {
+        "dimID": [
+          "20191116"
+        ],
+        "metricID": [
+          7740
+        ]
+      }
+    ]
   },
-  fields: {
-    dimID: [
+  "fields": {
+    "dimID": [
       {
-        id: 'qt_nzqx6a0xvb',
-        name: 'Age Group',
-        type: 'TEXT',
-        concept: 'DIMENSION',
-      },
+        "id": "qt_xu8rsecr3b",
+        "name": "Date",
+        "type": "YEAR_MONTH_DAY",
+        "concept": "DIMENSION"
+      }
     ],
-    metricID: [
+    "metricID": [
       {
-        id: 'qt_8isx6a0xvb',
-        name: 'Population',
-        type: 'NUMBER',
-        concept: 'METRIC',
-      },
-    ],
+        "id": "qt_eicxedcr3b",
+        "name": "Pageviews",
+        "type": "NUMBER",
+        "concept": "METRIC"
+      }
+    ]
   },
-  style: {},
+  "style": {},
+  "theme": {
+    "themeFillColor": {
+      "color": "#ffffff",
+      "themeRef": {
+        "index": 0
+      }
+    },
+    "themeFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 1
+      }
+    },
+    "themeFontFamily": "Roboto",
+    "themeAccentFillColor": {
+      "color": "#e0e0e0",
+      "themeRef": {
+        "index": 2
+      }
+    },
+    "themeAccentFontColor": {
+      "color": "#000000",
+      "themeRef": {
+        "index": 3
+      }
+    },
+    "themeAccentFontFamily": "Roboto",
+    "themeSeriesColor": [
+      {
+        "color": "#0072f0",
+        "seriesRef": {
+          "index": 0
+        },
+        "themeRef": {
+          "index": 4
+        }
+      },
+      {
+        "color": "#00b6cb",
+        "seriesRef": {
+          "index": 1
+        },
+        "themeRef": {
+          "index": 5
+        }
+      },
+      {
+        "color": "#f10096",
+        "seriesRef": {
+          "index": 2
+        },
+        "themeRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#f66d00",
+        "seriesRef": {
+          "index": 3
+        },
+        "themeRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ffa800",
+        "seriesRef": {
+          "index": 4
+        },
+        "themeRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#7cb342",
+        "seriesRef": {
+          "index": 5
+        },
+        "themeRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#5e35b1",
+        "seriesRef": {
+          "index": 6
+        }
+      },
+      {
+        "color": "#03a9f4",
+        "seriesRef": {
+          "index": 7
+        }
+      },
+      {
+        "color": "#ec407a",
+        "seriesRef": {
+          "index": 8
+        }
+      },
+      {
+        "color": "#ff7043",
+        "seriesRef": {
+          "index": 9
+        }
+      },
+      {
+        "color": "#737373",
+        "seriesRef": {
+          "index": 10
+        }
+      },
+      {
+        "color": "#f15a60",
+        "seriesRef": {
+          "index": 11
+        }
+      },
+      {
+        "color": "#7ac36a",
+        "seriesRef": {
+          "index": 12
+        }
+      },
+      {
+        "color": "#5a9bd4",
+        "seriesRef": {
+          "index": 13
+        }
+      },
+      {
+        "color": "#faa75a",
+        "seriesRef": {
+          "index": 14
+        }
+      },
+      {
+        "color": "#9e67ab",
+        "seriesRef": {
+          "index": 15
+        }
+      },
+      {
+        "color": "#ce7058",
+        "seriesRef": {
+          "index": 16
+        }
+      },
+      {
+        "color": "#d77fb3",
+        "seriesRef": {
+          "index": 17
+        }
+      },
+      {
+        "color": "#81d4fa",
+        "seriesRef": {
+          "index": 18
+        }
+      },
+      {
+        "color": "#f48fb1",
+        "seriesRef": {
+          "index": 19
+        }
+      }
+    ],
+    "themeIncreaseColor": {
+      "color": "#388e3c"
+    },
+    "themeDecreaseColor": {
+      "color": "#f44336"
+    },
+    "themeGridColor": {
+      "color": "#d1d1d1"
+    }
+  },
+  "interactions": {}
 };
